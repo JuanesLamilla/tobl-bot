02254ed2ada42109830e18d0a2702fc7fe4d370f
Juan Lamilla <juan.e.lamilla@gmail.com>
2020-03-13 18:02:06 -0400

    Create neccesary classes

---
+++ stattracker.py
@@ -0,0 +1,56 @@
+class team:
+    name = str
+    players: list
+    playrate_data: dict
+
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+        self.playrate_data = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: []}
+
+class player:
+    map_data: dict
+    name: str
+    team: team
+    playtime: dict
+    total_playtime: int
+
+    def __init__(self, name, team):
+        self.name = name
+        self.team = team
+        self.map_data = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: []}
+        self.playtime = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: []}
+        self.total_playtime = 0
+
+class map_round:
+    map_name: str
+    map_time: int
+    player_dict: dict
+    hero_playtime_1: list
+    hero_playtime_2: list
+
+    def __init__(self, map_name):
+        self.map_name = map_name
+        self.player_dict = {}
+        self.hero_playtime_1 = []
+        self.hero_playtime_2 = []
+
+
+class match:
+    teams: tuple
+    score: tuple
+    maps: list
+    week: int
+    
+    def __init__(self, teams, score, week):
+        self.teams = teams
+        self.score = score
+        self.maps = []
+        self.week = week
+        
+
+
+#PN0, PN1, PN2, PN3, PN4, PN5, PN6, PN7, PN8, PN9, PN10, PN11, Unknown, Global, HUD, HUD Time, Map, Match Time (Seconds), Start Up, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, Team 1 Playtime, Team 2 Playtime
+#0  , 1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  , 10  , 11  , 12     , 13    , 14 , 15      , 16 , 17                  , 18      , 19, 20, 21, 22, 23, 24, 25, 26, 27, 28 , 29 , 30             , 31
+#1428.128, Global, Falso, 0.05, La Habana, 1092, Verdadero, {54; 5; 16; 6201.54; 10331.24; 6276.69; 2819.84; 4}, {32; 8; 16; 4792.86; 8974.64; 5321.19; 1951.42; 6}, {80; 15; 16; 12809.45; 0; 12742.20; 4789.30; 5}, {62; 34; 8; 14346.65; 2528.07; 14980.90; 8853.40; 7}, {86; 41; 16; 12892.86; 0; 8031.22; 1880.78; 6}, {68; 22; 18; 14819.60; 2485.36; 8935.03; 4630.26; 7}, {38; 7; 23; 5677.94; 10066.64; 7215.66; 3439.31; 4}, {47; 14; 21; 8695.46; 5307.79; 6679.01; 2402.76; 4}, {60; 22; 21; 15117.47; 0; 15793.74; 6543.80; 7}, {57; 26; 20; 17242.04; 5114.05; 25734.34; 14658.11; 8}, {27; 8; 20; 4240.57; 307.20; 5069.53; 1592.95; 2; 27}, {35; 13; 20; 5275.31; 8385.21; 5332.26; 1720.93; 6}, {0; 0; 0; 0; 1092; 0; 0; 0; 0; 0; 0; 0; 0; 1092; 0; 0; 1092; 1092; 1092; 0; 0; 0; 0; 1092}, {0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1092; 0; 1092; 0; 0; 1092; 0; 1092; 0; 0; 1092; 0; 1092}, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+#{Team 1: [Players], Team 2: [Players]}
--- toblbot.py
+++ toblbot.py
@@ -1,7 +1,12 @@
 import discord
+import csv
+from stattracker import team, player, map_round, match
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 
+teams = []
+players = []
+matches = []
 
 client = commands.Bot(command_prefix = '$')
 client.remove_command('help')
@@ -38,8 +43,8 @@ async def standings(ctx):
     embed.set_footer(text="Last updated March 5th, 2020 at 7:13 pm.")
     await ctx.send(embed=embed)
 
-@client.command()
-async def KIT(ctx):
+@client.command(case_insensitive=True)
+async def kit(ctx):
     await ctx.send("KAT!")
 
 @client.command()
@@ -69,10 +74,6 @@ async def help(ctx):
     embed.add_field(name="$stats", value="Coming soon ;)", inline=True)
     embed.set_footer(text="More features are on the way!")
     await ctx.send(embed=embed)
-
-@client.command()
-async def stats(ctx):
-    await ctx.send("Game statistics coming soon.")
 
 @client.command()
 async def upcoming(ctx):
@@ -117,9 +118,104 @@ async def schedule(ctx):
                                             4:00pm (login @ 3:45pm) - Finer Things Club vs. Game Hive\n\
                                             6:00pm (login @ 5:45pm) - Fewbisoft vs. Onibaku\n\
                                             8:00pm (login @ 7:45pm) - Cronchers of Catan vs. Everything Hurts", inline=False)
-    embed.add_field(name="May 24th, 2020, 2020", value="TBA", inline=False)
+    embed.add_field(name="May 24th, 2020", value="TBA", inline=False)
     embed.add_field(name="May 31st, 2020", value="TBA", inline=False)
     embed.add_field(name="June 7th, 2020", value="TBA", inline=False)       
     await ctx.send(embed=embed)
 
+@client.command()
+async def stats_player(ctx, name: str):
+
+    for p in players:
+        if p.name.lower() == name.lower():
+            temp_minutes = p.total_playtime // 60
+            hours = str(temp_minutes // 60)
+            minutes = str(temp_minutes % 60)
+
+            total_maps = 0
+            for k in p.map_data.keys:
+                if p.map_data[k] != []:
+                    total_maps += len(p.map_data[k])
+
+            embed=discord.Embed(title="Stats for " + name + " (" + p.team.name + ")", description=name + " has played for a total of " + hours + " hours and " + minutes + " minutes across " + total_maps + " maps.", color=0xf3e91d)
+            embed.set_thumbnail(url="http://overwatchtoronto.org/images/logo_white.png")
+            embed.add_field(name="Eliminations", value="Total: 0\nAvg/10mins: 0", inline=True)
+            embed.add_field(name="Final Blows", value="Total: 0\nAvg/10mins: 0", inline=True)
+            embed.add_field(name="Deaths", value="Total: 0\nAvg/10mins: 0", inline=True)
+            embed.add_field(name="Damage Dealt", value="Total: 0\nAvg/10mins: 0", inline=True)
+            embed.add_field(name="Healing Dealt", value="Total: 0\nAvg/10mins: 0", inline=True)
+            embed.add_field(name="Damage Received", value="Total: 0\nAvg/10mins: 0", inline=True)
+            embed.add_field(name="Healing Received", value="Total: 0\nAvg/10mins: 0", inline=True)
+            embed.add_field(name="Ults Used", value="Total: 0\nAvg/10mins: 0", inline=True)
+            embed.add_field(name="Tactical Crouches", value="Total: 0\nAvg/10mins: 0", inline=True)
+            embed.set_footer(text="Last updated on March 13, 2020")
+            await ctx.send(embed=embed)
+
+            return
+    
+    await ctx.send("Unable to find player with name: " + name)
+
+def scan_data():
+    with open('teams.txt', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, skipinitialspace=True, delimiter=',')
+        for row in csv_reader:
+            curr_team = team(row[0])
+            for name in row[1:]:
+                curr_player = player(name, curr_team)
+                players.append(curr_player)
+                curr_team.players.append(curr_player)
+            teams.append(curr_team)
+
+
+    with open('stats.txt', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, skipinitialspace=True, delimiter=',')
+        for row in csv_reader:
+
+            if row[0] == "GAME":
+                for t in teams:
+                    if t.name == row[2]:
+                        temp_team_1 = t
+                    elif t.name == row[3]:
+                        temp_team_2 = t
+                match_save = match((temp_team_1, temp_team_2), (int(row[4]), int(row[5])), int(row[1]))
+
+            elif len(row) != 142:
+                raise ValueError("Invalid map input")
+
+            else:
+            
+                current_round = map_round(row[16])
+
+                for x in range(12):
+                    player_stats = row[x + 19].replace("{", "").replace("}", "").replace(" ", "").split(";")
+                    current_round.player_dict[row[x]] = player_stats
+
+                    while len(player_stats) < 9:
+                        player_stats.append(0)
+
+                    for y in players:
+                        if y.name == row[x]:
+                            y.map_data[match_save.week].append(player_stats)
+                            y.playtime[match_save.week].append(int(row[17]))
+                            y.total_playtime += int(row[17])
+                    
+
+                temp_playtime = [int(i.replace(" ", "")) for i in row[31].replace("{", "").replace("}", "").split(";")]
+                while len(temp_playtime) < 31:
+                    temp_playtime.append(0)
+                current_round.hero_playtime_1 = temp_playtime
+                match_save.teams[0].playrate_data[match_save.week].append(temp_playtime)
+
+                temp_playtime = [int(i.replace(" ", "")) for i in row[32].replace("{", "").replace("}", "").split(";")]
+                while len(temp_playtime) < 31:
+                    temp_playtime.append(0)
+                current_round.hero_playtime_2 = temp_playtime
+                match_save.teams[1].playrate_data[match_save.week].append(temp_playtime)
+
+                current_round.map_time = int(row[17])
+
+                match_save.maps.append(current_round)
+                matches.append(match_save)
+
+scan_data()
 client.run('Njg1MTg5OTA3OTQwOTAwODY5.XmFFGA.5Rg5_RrWeboBw9LQ6XGWNbf8BL8')
