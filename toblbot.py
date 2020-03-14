cd7f5a69298efe57d8ca03b7678fce1a36023b4c
Juan Lamilla <juan.e.lamilla@gmail.com>
2020-03-14 12:10:35 -0400

    Add individual stat tracker

--- toblbot.py
+++ toblbot.py
@@ -1,5 +1,6 @@
 import discord
 import csv
+import pathlib
 from stattracker import team, player, map_round, match
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
@@ -7,6 +8,7 @@ from discord.ext.commands import CommandNotFound
 teams = []
 players = []
 matches = []
+last_updated = "Last updated March 13th, 2020 at 7:22 pm."
 
 client = commands.Bot(command_prefix = '$')
 client.remove_command('help')
@@ -40,7 +42,7 @@ async def standings(ctx):
     embed.add_field(name="6. Stacy's Moms", value="W: 0 L: 0 D: 0 Diff: +0", inline=False)
     embed.add_field(name="7. Fewbisoft", value="W: 0 L: 0 D: 0 Diff: +0", inline=False)
     embed.add_field(name="8. Nerf Mei", value="W: 0 L: 0 D: 0 Diff: +0", inline=False)
-    embed.set_footer(text="Last updated March 5th, 2020 at 7:13 pm.")
+    embed.set_footer(text=last_updated)
     await ctx.send(embed=embed)
 
 @client.command(case_insensitive=True)
@@ -124,39 +126,189 @@ async def schedule(ctx):
     await ctx.send(embed=embed)
 
 @client.command()
-async def stats_player(ctx, name: str):
+async def team_playtime(ctx, team_name=None):
+    if team_name is None:
+        await ctx.send("Missing argument: Team Name\n(ex: '$stats_player StacysMoms')")
+        return
+    
+    total_time = 0
+    reaper = 0
+    tracer = 0
+    mercy = 0
+    hanzo = 0
+    torbjorn = 0
+    reinhardt = 0
+    pharah = 0
+    winston = 0
+    widowmaker = 0
+    bastion = 0
+    symmetra = 0
+    zenyatta = 0
+    genji = 0
+    roadhog = 0
+    mccree = 0
+    junkrat = 0
+    zarya = 0
+    soldier = 0
+    lucio = 0
+    dva = 0
+    mei = 0
+    sombra = 0
+    doomfist = 0
+    ana = 0
+    orisa = 0
+    brigitte = 0
+    moira = 0
+    wrecking_ball = 0
+    ashe = 0
+    baptiste = 0
+    sigma = 0
+
+    all_heroes = [Ana, Ashe, Bastion -
+Brigitte -
+D.va -
+Doomfist -
+Genji -
+Hanzo -
+Junkrat -
+Lucio -
+Mccree -
+Mei -
+Mercy -
+Moira -
+Orisa -
+Pharah -
+Reaper -
+Reinhardt -
+Roadhog -
+Soldier 76 -
+Sombra -
+Symmetra -
+Torbjorn -
+Tracer -
+Widowmaker -
+Winston -
+Wrecking Ball -
+Zarya -
+Zenyatta -]
+    
+    for t in teams:
+        if team_name.lower().replace("'", "") == t.name.lower().replace("'", "").replace(" ", ""):
+            for k in t.playrate_data.keys():
+                for elem in t.playrate_data[k]:
+                    total_time += elem[31]
+                    reaper += elem[0]
+                    tracer += elem[1]
+                    mercy += elem[2]
+                    hanzo += elem[3]
+                    torbjorn += elem[4]
+                    reinhardt += elem[5]
+                    pharah += elem[6]
+                    winston += elem[7]
+                    widowmaker += elem[8]
+                    bastion += elem[9]
+                    symmetra += elem[10]
+                    zenyatta += elem[11]
+                    genji += elem[12]
+                    roadhog += elem[13]
+                    mccree += elem[14]
+                    junkrat += elem[15]
+                    zarya += elem[16]
+                    soldier += elem[17]
+                    lucio += elem[18]
+                    dva += elem[19]
+                    mei += elem[20]
+                    sombra += elem[21]
+                    doomfist += elem[22]
+                    ana += elem[23]
+                    orisa += elem[24]
+                    brigitte += elem[25]
+                    moira += elem[26]
+                    wrecking_ball += elem[27]
+                    ashe += elem[28]
+                    baptiste += elem[29]
+                    sigma += elem[30]
+
+
+            embed=discord.Embed(title="Hero playtime for  " + t.name, description="", color=0xf3e91d)
+            embed.set_thumbnail(url="http://overwatchtoronto.org/images/logo_white.png")
+            embed.add_field(name="Eliminations", value="Total: " + str(total_elims) + "\nAvg/10mins: " + "{0:.2f}".format(total_elims*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Final Blows", value="Total: " + str(total_final_blows) + "\nAvg/10mins: " + "{0:.2f}".format(total_final_blows*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Deaths", value="Total: " + str(total_deaths) + "\nAvg/10mins: " + "{0:.2f}".format(total_deaths*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Damage Dealt", value="Total: " + str(total_damage_dealt) + "\nAvg/10mins: " + "{0:.2f}".format(total_damage_dealt*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Healing Dealt", value="Total: " + str(total_healing_dealt) + "\nAvg/10mins: " + "{0:.2f}".format(total_healing_dealt*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Damage Received", value="Total: " + str(total_damage_taken) + "\nAvg/10mins: " + "{0:.2f}".format(total_damage_taken*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Healing Received", value="Total: " + str(total_healing_taken) + "\nAvg/10mins: " + "{0:.2f}".format(total_healing_taken*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Ults Used", value="Total: " + str(total_ults_used) + "\nAvg/10mins: " + "{0:.2f}".format(total_ults_used*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Tactical Crouches", value="Total: " + str(total_crouches) + "\nAvg/10mins: " + "{0:.2f}".format(total_crouches*(10/temp_minutes)), inline=True)
+            embed.set_footer(text=last_updated)
+            await ctx.send(embed=embed)
+            return
+    
+    await ctx.send("Unable to find team with name: " + team_name + "\nRemember to omit spaces ('Stacy's Moms' -> 'Stacy'sMoms')")
+
+@client.command()
+async def stats_player(ctx, name=None):
+    if name is None:
+        await ctx.send("Missing argument: Player Name\n(ex: '$stats_player Krusher99')")
+        return
 
     for p in players:
         if p.name.lower() == name.lower():
+            #Time Played
             temp_minutes = p.total_playtime // 60
             hours = str(temp_minutes // 60)
             minutes = str(temp_minutes % 60)
 
             total_maps = 0
-            for k in p.map_data.keys:
+            total_elims = 0
+            total_final_blows = 0
+            total_deaths = 0
+            total_damage_dealt = 0
+            total_healing_dealt = 0
+            total_damage_taken = 0
+            total_healing_taken = 0
+            total_ults_used = 0
+            total_crouches = 0
+            for k in p.map_data.keys():
+                           
+                #Maps Played
                 if p.map_data[k] != []:
                     total_maps += len(p.map_data[k])
+                
+                #{60; 22; 21; 15117.47; 0; 15793.74; 6543.80; 7}
+                for elem in p.map_data[k]:
+                    total_elims += int(elem[0])
+                    total_final_blows += int(elem[1])
+                    total_deaths += int(elem[2])
+                    total_damage_dealt += float(elem[3])
+                    total_healing_dealt += float(elem[4])
+                    total_damage_taken += float(elem[5])
+                    total_healing_taken += float(elem[6])
+                    total_ults_used += int(elem[7])
+                    total_crouches += int(elem[8])
+            
 
-            embed=discord.Embed(title="Stats for " + name + " (" + p.team.name + ")", description=name + " has played for a total of " + hours + " hours and " + minutes + " minutes across " + total_maps + " maps.", color=0xf3e91d)
+
+            embed=discord.Embed(title="Stats for " + p.name + " (" + p.team.name + ")", description=p.name + " has played for a total of " + hours + " hours and " + minutes + " minutes across " + str(total_maps) + " map(s).", color=0xf3e91d)
             embed.set_thumbnail(url="http://overwatchtoronto.org/images/logo_white.png")
-            embed.add_field(name="Eliminations", value="Total: 0\nAvg/10mins: 0", inline=True)
-            embed.add_field(name="Final Blows", value="Total: 0\nAvg/10mins: 0", inline=True)
-            embed.add_field(name="Deaths", value="Total: 0\nAvg/10mins: 0", inline=True)
-            embed.add_field(name="Damage Dealt", value="Total: 0\nAvg/10mins: 0", inline=True)
-            embed.add_field(name="Healing Dealt", value="Total: 0\nAvg/10mins: 0", inline=True)
-            embed.add_field(name="Damage Received", value="Total: 0\nAvg/10mins: 0", inline=True)
-            embed.add_field(name="Healing Received", value="Total: 0\nAvg/10mins: 0", inline=True)
-            embed.add_field(name="Ults Used", value="Total: 0\nAvg/10mins: 0", inline=True)
-            embed.add_field(name="Tactical Crouches", value="Total: 0\nAvg/10mins: 0", inline=True)
-            embed.set_footer(text="Last updated on March 13, 2020")
+            embed.add_field(name="Eliminations", value="Total: " + str(total_elims) + "\nAvg/10mins: " + "{0:.2f}".format(total_elims*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Final Blows", value="Total: " + str(total_final_blows) + "\nAvg/10mins: " + "{0:.2f}".format(total_final_blows*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Deaths", value="Total: " + str(total_deaths) + "\nAvg/10mins: " + "{0:.2f}".format(total_deaths*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Damage Dealt", value="Total: " + str(total_damage_dealt) + "\nAvg/10mins: " + "{0:.2f}".format(total_damage_dealt*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Healing Dealt", value="Total: " + str(total_healing_dealt) + "\nAvg/10mins: " + "{0:.2f}".format(total_healing_dealt*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Damage Received", value="Total: " + str(total_damage_taken) + "\nAvg/10mins: " + "{0:.2f}".format(total_damage_taken*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Healing Received", value="Total: " + str(total_healing_taken) + "\nAvg/10mins: " + "{0:.2f}".format(total_healing_taken*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Ults Used", value="Total: " + str(total_ults_used) + "\nAvg/10mins: " + "{0:.2f}".format(total_ults_used*(10/temp_minutes)), inline=True)
+            embed.add_field(name="Tactical Crouches", value="Total: " + str(total_crouches) + "\nAvg/10mins: " + "{0:.2f}".format(total_crouches*(10/temp_minutes)), inline=True)
+            embed.set_footer(text=last_updated)
             await ctx.send(embed=embed)
-
             return
     
     await ctx.send("Unable to find player with name: " + name)
 
 def scan_data():
-    with open('teams.txt', 'r') as csv_file:
+    with open(str(pathlib.Path(__file__).parent.absolute()) + "\\teams.txt", 'r') as csv_file:
         csv_reader = csv.reader(csv_file, skipinitialspace=True, delimiter=',')
         for row in csv_reader:
             curr_team = team(row[0])
@@ -167,7 +319,7 @@ def scan_data():
             teams.append(curr_team)
 
 
-    with open('stats.txt', 'r') as csv_file:
+    with open(str(pathlib.Path(__file__).parent.absolute()) + "\\stats.txt", 'r') as csv_file:
         csv_reader = csv.reader(csv_file, skipinitialspace=True, delimiter=',')
         for row in csv_reader:
 
@@ -203,12 +355,14 @@ def scan_data():
                 temp_playtime = [int(i.replace(" ", "")) for i in row[31].replace("{", "").replace("}", "").split(";")]
                 while len(temp_playtime) < 31:
                     temp_playtime.append(0)
+                temp_playtime.append(int(row[17]))
                 current_round.hero_playtime_1 = temp_playtime
                 match_save.teams[0].playrate_data[match_save.week].append(temp_playtime)
 
                 temp_playtime = [int(i.replace(" ", "")) for i in row[32].replace("{", "").replace("}", "").split(";")]
                 while len(temp_playtime) < 31:
                     temp_playtime.append(0)
+                temp_playtime.append(int(row[17]))
                 current_round.hero_playtime_2 = temp_playtime
                 match_save.teams[1].playrate_data[match_save.week].append(temp_playtime)
 
